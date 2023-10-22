bcbcf41c7354a3438f6ea4f6bba32b83019e4218
Mbr90 <49838649+Mbr90@users.noreply.github.com>
2023-10-22 13:05:11 +0200

    Update testInvalidCredentials.cy.js

--- cypress/e2e/tests/testInvalidCredentials.cy.js
+++ cypress/e2e/tests/testInvalidCredentials.cy.js
@@ -1,30 +1,4 @@
-// describe("Login in with inalid credentials", () => {
-//   it("prevents the user form logging in and presents an readable error", () => {
-//     cy.visit("/");
-//     cy.wait(1000);
-//     cy.get(`.modal-footer [data-bs-target="#loginModal"]`).click();
-//     cy.wait(1000);
-//     cy.get("#loginEmail").type("invalidCredentials{enter}");
-//     cy.wait(1000);
-//     cy.get("#loginEmail").clear();
-//     cy.wait(1000);
-//     cy.get("#loginPassword").type("notAPassword{enter}");
-//     cy.wait(1000);
-//     cy.get("#loginPassword").clear();
-//     cy.wait(1000);
-//     cy.get("#loginEmail").type("invalidCredentials@email.com");
-//     cy.wait(1000);
-//     cy.get("#loginPassword").type("notAPassword{enter}");
-//     cy.wait(200);
-//     cy.on("window:alert", (str) => {
-//       expect(str).to.equal(
-//         "Only Dette er bare tull student or  emails are valid."
-//       );
-//     });
-//   });
-// });
-
-describe("Login in with inalid credentials", () => {
+describe("Login in with invalid credentials", () => {
   it("prevents the user form logging in and presents an readable error", () => {
     cy.visit("/");
     cy.wait(1000);
@@ -32,11 +6,9 @@ describe("Login in with inalid credentials", () => {
     cy.wait(1000);
     cy.get("#loginEmail").type("invalidCredentials@email.com");
     cy.get("#loginPassword").type("notAPassword{enter}");
-    cy.wait(500);
-    cy.get("#loginEmail")
-      .invoke("attr", "title")
-      .then((title) => {
-        expect(title).to.exist;
-      });
+    cy.wait(100);
+    cy.on("window:alert", (str) => {
+      expect(str).to.equal("Only Noroff student or teacher emails are valid.");
+    });
   });
 });
